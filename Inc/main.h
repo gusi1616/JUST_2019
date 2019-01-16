551d12442b1dc5a7fa898ad6f57dfcf384e78866
truongdaniel <44634283+truongdaniel@users.noreply.github.com>
2019-01-16 14:26:23 +0100

    adsda

--- Inc/main.h
+++ Inc/main.h
@@ -1,3 +1,4 @@
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.h
@@ -10,7 +11,7 @@
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * COPYRIGHT(c) 2018 STMicroelectronics
+  * COPYRIGHT(c) 2019 STMicroelectronics
   *
   * Redistribution and use in source and binary forms, with or without modification,
   * are permitted provided that the following conditions are met:
@@ -36,39 +37,70 @@
   *
   ******************************************************************************
   */
+/* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
 
+/* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stm32f4xx.h"
 /* USER CODE END Includes */
 
-/* Private define ------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
 
-#define WINC_IRQ_Pin GPIO_PIN_0
-#define WINC_IRQ_GPIO_Port GPIOA
-#define WINC_CFG_Pin GPIO_PIN_1
-#define WINC_CFG_GPIO_Port GPIOA
-#define WINC_EN_Pin GPIO_PIN_2
-#define WINC_EN_GPIO_Port GPIOA
-#define WINC_RST_Pin GPIO_PIN_3
-#define WINC_RST_GPIO_Port GPIOA
-#define WINC_CS_Pin GPIO_PIN_4
-#define WINC_CS_GPIO_Port GPIOA
-#define WINC_WAKE_Pin GPIO_PIN_10
-#define WINC_WAKE_GPIO_Port GPIOA
+/* USER CODE END ET */
 
-/* ########################## Assert Selection ############################## */
-/**
-  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
-  *        HAL drivers code
-  */
-/* #define USE_FULL_ASSERT    1U */
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
 
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define WINC_CS_Pin GPIO_PIN_6
+#define WINC_CS_GPIO_Port GPIOC
+#define WINC_EN_Pin GPIO_PIN_7
+#define WINC_EN_GPIO_Port GPIOC
+#define WINC_RST_Pin GPIO_PIN_8
+#define WINC_RST_GPIO_Port GPIOC
+#define WINC_IRQ_Pin GPIO_PIN_9
+#define WINC_IRQ_GPIO_Port GPIOC
+#define WINC_IRQ_EXTI_IRQn EXTI9_5_IRQn
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 #define WINC_GPIO_Port GPIOA
 
@@ -90,15 +122,9 @@ typedef union{
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
- extern "C" {
-#endif
-void _Error_Handler(char *, int);
-
-#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
-#ifdef __cplusplus
 }
 #endif
 
-#endif /* __MAIN_H__ */
+#endif /* __MAIN_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
