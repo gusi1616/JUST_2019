a6d4edc6d9d9f9be1dc3b9ce9351ab787555d99a
truongdaniel <44634283+truongdaniel@users.noreply.github.com>
2019-01-16 13:49:01 +0100

    a

--- Src/main.c
+++ Src/main.c
@@ -164,19 +164,93 @@ static void MX_UART4_Init(void);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
-void wifi_cb(uint8 u8WiFiEvent, void * pvMsg)
+static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
 {
-	switch(u8WiFiEvent)
+	switch (u8MsgType) {
+	case M2M_WIFI_RESP_SCAN_DONE:
 	{
+		tstrM2mScanDone *pstrInfo = (tstrM2mScanDone *)pvMsg;
+		scan_request_index = 0;
+		if (pstrInfo->u8NumofCh >= 1) {
+			m2m_wifi_req_scan_result(scan_request_index);
+			scan_request_index++;
+		} else {
+			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+		}
+
+		break;
+	}
+
+	case M2M_WIFI_RESP_SCAN_RESULT:
+	{
+		tstrM2mWifiscanResult *pstrScanResult = (tstrM2mWifiscanResult *)pvMsg;
+		uint16_t demo_ssid_len;
+		uint16_t scan_ssid_len = strlen((const char *)pstrScanResult->au8SSID);
+
+		/* display founded AP. */
+		printf("[%d] SSID:%s\r\n", scan_request_index, pstrScanResult->au8SSID);
+
+		num_founded_ap = m2m_wifi_get_num_ap_found();
+		if (scan_ssid_len) {
+			/* check same SSID. */
+			demo_ssid_len = strlen((const char *)MAIN_WLAN_SSID);
+			if
+			(
+				(demo_ssid_len == scan_ssid_len) &&
+				(!memcmp(pstrScanResult->au8SSID, (uint8_t *)MAIN_WLAN_SSID, demo_ssid_len))
+			) {
+				/* A scan result matches an entry in the preferred AP List.
+				 * Initiate a connection request.
+				 */
+				printf("Found %s \r\n", MAIN_WLAN_SSID);
+				m2m_wifi_connect((char *)MAIN_WLAN_SSID,
+						sizeof(MAIN_WLAN_SSID),
+						MAIN_WLAN_AUTH,
+						(void *)MAIN_WLAN_PSK,
+						M2M_WIFI_CH_ALL);
+				break;
+			}
+		}
+
+		if (scan_request_index < num_founded_ap) {
+			m2m_wifi_req_scan_result(scan_request_index);
+			scan_request_index++;
+		} else {
+			printf("can not find AP %s\r\n", MAIN_WLAN_SSID);
+			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+		}
+
+		break;
+	}
+
+	case M2M_WIFI_RESP_CON_STATE_CHANGED:
+	{
+		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
+		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
+			m2m_wifi_request_dhcp_client();
+		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
+			printf("Wi-Fi disconnected\r\n");
+
+			/* Request scan. */
+			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+		}
+
+		break;
+	}
+
 	case M2M_WIFI_REQ_DHCP_CONF:
 	{
-		uint8 *pu8IPAddress = (uint8*)pvMsg;
-		printf("Associated STA has IP Address \"%u.%u.%u.%u\"\n", pu8IPAddress[0],
-				pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
-	}
-	break;
-	default:
+		uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
+		printf("Wi-Fi connected\r\n");
+		printf("Wi-Fi IP is %u.%u.%u.%u\r\n",
+				pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
 		break;
+	}
+
+	default:
+	{
+		break;
+	}
 	}
 }
 
--- Src/winc1500.c
+++ Src/winc1500.c
@@ -27,208 +27,92 @@
 
 static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
 {
-	switch (u8MsgType) {
-	case M2M_WIFI_RESP_DEFAULT_CONNECT:
-	{
-		tstrM2MDefaultConnResp *pstrDefaultConnResp = (tstrM2MDefaultConnResp *)pvMsg;
-		if (pstrDefaultConnResp->s8ErrorCode) {
-			_status = WL_DISCONNECTED;
-		}
-	}
-	break;
-
-	case M2M_WIFI_RESP_CON_STATE_CHANGED:
-	{
-		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
-		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
-			//SERIAL_PORT_MONITOR.println("wifi_cb: M2M_WIFI_RESP_CON_STATE_CHANGED: CONNECTED");
-			if (_mode == WL_STA_MODE && !_dhcp) {
-				_status = WL_CONNECTED;
-
-#ifdef CONF_PERIPH
-				// WiFi led ON.
-				m2m_periph_gpio_set_val(M2M_PERIPH_GPIO15, 0);
-#endif
-			} else if (_mode == WL_AP_MODE) {
-				_status = WL_AP_CONNECTED;
-			}
-		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
-			//SERIAL_PORT_MONITOR.println("wifi_cb: M2M_WIFI_RESP_CON_STATE_CHANGED: DISCONNECTED");
-			if (_mode == WL_STA_MODE) {
-				_status = WL_DISCONNECTED;
-				if (_dhcp) {
-					_localip = 0;
-					_submask = 0;
-					_gateway = 0;
-				}
-				// Close sockets to clean state
-				// Clients will need to reconnect once the physical link will be re-established
-				for (int i = 0; i < MAX_SOCKET; i++) {
-					WiFiSocket.close(i);
-				}
-			} else if (_mode == WL_AP_MODE) {
-				_status = WL_AP_LISTENING;
-			}
-#ifdef CONF_PERIPH
-			// WiFi led OFF (rev A then rev B).
-			m2m_periph_gpio_set_val(M2M_PERIPH_GPIO15, 1);
-			m2m_periph_gpio_set_val(M2M_PERIPH_GPIO4, 1);
-#endif
-		}
-	}
-	break;
-
-	case M2M_WIFI_REQ_DHCP_CONF:
-	{
-		if (_mode == WL_STA_MODE) {
-			tstrM2MIPConfig *pstrIPCfg = (tstrM2MIPConfig *)pvMsg;
-			_localip = pstrIPCfg->u32StaticIP;
-			_submask = pstrIPCfg->u32SubnetMask;
-			_gateway = pstrIPCfg->u32Gateway;
-
-			_status = WL_CONNECTED;
-
-#ifdef CONF_PERIPH
-			// WiFi led ON (rev A then rev B).
-			m2m_periph_gpio_set_val(M2M_PERIPH_GPIO15, 0);
-			m2m_periph_gpio_set_val(M2M_PERIPH_GPIO4, 0);
-#endif
-		}
-		/*uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
-				SERIAL_PORT_MONITOR.print("wifi_cb: M2M_WIFI_REQ_DHCP_CONF: IP is ");
-				SERIAL_PORT_MONITOR.print(pu8IPAddress[0], 10);
-				SERIAL_PORT_MONITOR.print(".");
-				SERIAL_PORT_MONITOR.print(pu8IPAddress[1], 10);
-				SERIAL_PORT_MONITOR.print(".");
-				SERIAL_PORT_MONITOR.print(pu8IPAddress[2], 10);
-				SERIAL_PORT_MONITOR.print(".");
-				SERIAL_PORT_MONITOR.print(pu8IPAddress[3], 10);
-				SERIAL_PORT_MONITOR.println("");*/
-	}
-	break;
-
-	case M2M_WIFI_RESP_CURRENT_RSSI:
-	{
-		_resolve = *((int8_t *)pvMsg);
-	}
-	break;
-
-	case M2M_WIFI_RESP_PROVISION_INFO:
-	{
-		tstrM2MProvisionInfo *pstrProvInfo = (tstrM2MProvisionInfo *)pvMsg;
-
-		if (pstrProvInfo->u8Status == M2M_SUCCESS) {
-			memset(_ssid, 0, M2M_MAX_SSID_LEN);
-			memcpy(_ssid, (char *)pstrProvInfo->au8SSID, strlen((char *)pstrProvInfo->au8SSID));
-			_mode = WL_STA_MODE;
-			_localip = 0;
-			_submask = 0;
-			_gateway = 0;
-			m2m_wifi_connect((char *)pstrProvInfo->au8SSID, strlen((char *)pstrProvInfo->au8SSID),
-					pstrProvInfo->u8SecType, pstrProvInfo->au8Password, M2M_WIFI_CH_ALL);
-		} else {
-			_status = WL_PROVISIONING_FAILED;
-			beginProvision();
-		}
-	}
-	break;
-
-	case M2M_WIFI_RESP_SCAN_DONE:
-	{
-		tstrM2mScanDone *pstrInfo = (tstrM2mScanDone *)pvMsg;
-		if (pstrInfo->u8NumofCh >= 1) {
-			_status = WL_SCAN_COMPLETED;
-		}
-	}
-	break;
-
-	case M2M_WIFI_RESP_SCAN_RESULT:
-	{
-		tstrM2mWifiscanResult *pstrScanResult = (tstrM2mWifiscanResult *)pvMsg;
-		uint16_t scan_ssid_len = strlen((const char *)pstrScanResult->au8SSID);
-		memset(_scan_ssid, 0, M2M_MAX_SSID_LEN);
-		if (scan_ssid_len) {
-			memcpy(_scan_ssid, (const char *)pstrScanResult->au8SSID, scan_ssid_len);
-		}
-		if (_remoteMacAddress) {
-			// reverse copy the remote MAC
-			for(int i = 0; i < 6; i++) {
-				_remoteMacAddress[i] = pstrScanResult->au8BSSID[5-i];
-			}
-		}
-		_resolve = pstrScanResult->s8rssi;
-		_scan_auth = pstrScanResult->u8AuthType;
-		_scan_channel = pstrScanResult->u8ch;
-		_status = WL_SCAN_COMPLETED;
-	}
-	break;
-
-	case M2M_WIFI_RESP_CONN_INFO:
-	{
-		tstrM2MConnInfo	*pstrConnInfo = (tstrM2MConnInfo*)pvMsg;
-
-		if (_remoteMacAddress) {
-			// reverse copy the remote MAC
-			for(int i = 0; i < 6; i++) {
-				_remoteMacAddress[i] = pstrConnInfo->au8MACAddress[5-i];
-			}
-			_remoteMacAddress = 0;
-		}
-
-		strcpy((char *)_ssid, pstrConnInfo->acSSID);
-	}
-	break;
-
-	case M2M_WIFI_RESP_GET_SYS_TIME:
-	{
-		if (_resolve != 0) {
-			memcpy((tstrSystemTime *)_resolve, pvMsg, sizeof(tstrSystemTime));
-
-			_resolve = 0;
-		}
-	}
-	break;
-
-	default:
-		break;
-	}
-}
-
-static void socket_cb(SOCKET sock, uint8 u8Msg, void *pvMsg)
-{
-	eventCallback(sock, u8Msg, pvMsg);
-}
-
-void ping_cb(uint32 u32IPAddr, uint32 u32RTT, uint8 u8ErrorCode)
-{
-	handlePingResponse(u32IPAddr, u32RTT, u8ErrorCode);
-}
-
-void handlePingResponse(uint32 u32IPAddr, uint32 u32RTT, uint8 u8ErrorCode)
-{
-	if (PING_ERR_SUCCESS == u8ErrorCode) {
-		// Ensure this ICMP reply comes from requested IP address
-		if (_resolve == u32IPAddr) {
-			_resolve = (uint32_t)u32RTT;
-		} else {
-			// Another network device replied to the our ICMP request
-			_resolve = (uint32_t)WL_PING_DEST_UNREACHABLE;
-		}
-	} else if (PING_ERR_DEST_UNREACH == u8ErrorCode) {
-		_resolve = (uint32_t)WL_PING_DEST_UNREACHABLE;
-	} else if (PING_ERR_TIMEOUT == u8ErrorCode) {
-		_resolve = (uint32_t)WL_PING_TIMEOUT;
-	} else {
-		_resolve = (uint32_t)WL_PING_ERROR;
-	}
-}
-
-void setPins(int8_t cs, int8_t irq, int8_t rst, int8_t en)
-{
-	gi8Winc1501CsPin = cs;
-	gi8Winc1501IntnPin = irq;
-	gi8Winc1501ResetPin = rst;
-	gi8Winc1501ChipEnPin = en;
+//	switch (u8MsgType) {
+//	case M2M_WIFI_RESP_SCAN_DONE:
+//	{
+//		tstrM2mScanDone *pstrInfo = (tstrM2mScanDone *)pvMsg;
+//		scan_request_index = 0;
+//		if (pstrInfo->u8NumofCh >= 1) {
+//			m2m_wifi_req_scan_result(scan_request_index);
+//			scan_request_index++;
+//		} else {
+//			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+//		}
+//
+//		break;
+//	}
+//
+//	case M2M_WIFI_RESP_SCAN_RESULT:
+//	{
+//		tstrM2mWifiscanResult *pstrScanResult = (tstrM2mWifiscanResult *)pvMsg;
+//		uint16_t demo_ssid_len;
+//		uint16_t scan_ssid_len = strlen((const char *)pstrScanResult->au8SSID);
+//
+//		/* display founded AP. */
+//		printf("[%d] SSID:%s\r\n", scan_request_index, pstrScanResult->au8SSID);
+//
+//		num_founded_ap = m2m_wifi_get_num_ap_found();
+//		if (scan_ssid_len) {
+//			/* check same SSID. */
+//			demo_ssid_len = strlen((const char *)MAIN_WLAN_SSID);
+//			if
+//			(
+//					(demo_ssid_len == scan_ssid_len) &&
+//					(!memcmp(pstrScanResult->au8SSID, (uint8_t *)MAIN_WLAN_SSID, demo_ssid_len))
+//			) {
+//				/* A scan result matches an entry in the preferred AP List.
+//				 * Initiate a connection request.
+//				 */
+//				printf("Found %s \r\n", MAIN_WLAN_SSID);
+//				m2m_wifi_connect((char *)MAIN_WLAN_SSID,
+//						sizeof(MAIN_WLAN_SSID),
+//						MAIN_WLAN_AUTH,
+//						(void *)MAIN_WLAN_PSK,
+//						M2M_WIFI_CH_ALL);
+//				break;
+//			}
+//		}
+//
+//		if (scan_request_index < num_founded_ap) {
+//			m2m_wifi_req_scan_result(scan_request_index);
+//			scan_request_index++;
+//		} else {
+//			printf("can not find AP %s\r\n", MAIN_WLAN_SSID);
+//			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+//		}
+//
+//		break;
+//	}
+//
+//	case M2M_WIFI_RESP_CON_STATE_CHANGED:
+//	{
+//		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
+//		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
+//			m2m_wifi_request_dhcp_client();
+//		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
+//			printf("Wi-Fi disconnected\r\n");
+//
+//			/* Request scan. */
+//			m2m_wifi_request_scan(M2M_WIFI_CH_ALL);
+//		}
+//
+//		break;
+//	}
+//
+//	case M2M_WIFI_REQ_DHCP_CONF:
+//	{
+//		uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
+//		printf("Wi-Fi connected\r\n");
+//		printf("Wi-Fi IP is %u.%u.%u.%u\r\n",
+//				pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
+//		break;
+//	}
+//
+//	default:
+//	{
+//		break;
+//	}
+//	}
 }
 
 int8_t wincInit(void)
@@ -279,7 +163,6 @@ int8_t wincStartConnect(const char *ssid, uint8_t u8SecType, const void *pvAuthI
 {
 	// Initialize socket API and register socket callback:
 	socketInit();
-	registerSocketCallback(socket_cb, resolve_cb);
 
 	int8_t status = m2m_wifi_connect((char*)ssid, sizeof(ssid), u8SecType,
 			(void*)pvAuthInfo, channel);
@@ -381,69 +264,6 @@ void set_device_name(const char* name)
 	m2m_wifi_set_device_name((uint8 *)name, strlen(name));
 }
 
-void disconnect()
-{
-	// Close sockets to clean state
-	for (int i = 0; i < MAX_SOCKET; i++) {
-		WiFiSocket.close(i);
-	}
-
-	m2m_wifi_disconnect();
-
-#ifdef CONF_PERIPH
-	// WiFi led OFF (rev A then rev B).
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO15, 1);
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO4, 1);
-#endif
-}
-
-void end()
-{
-	// Close sockets to clean state
-	for (int i = 0; i < MAX_SOCKET; i++) {
-		WiFiSocket.close(i);
-	}
-
-	if (_mode == WL_AP_MODE) {
-		m2m_wifi_disable_ap();
-	} else {
-		if (_mode == WL_PROV_MODE) {
-			m2m_wifi_stop_provision_mode();
-		}
-
-		m2m_wifi_disconnect();
-	}
-
-#ifdef CONF_PERIPH
-	// WiFi led OFF (rev A then rev B).
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO15, 1);
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO4, 1);
-#endif
-
-	socketDeinit();
-
-	m2m_wifi_deinit(NULL);
-
-	nm_bsp_deinit();
-
-	_mode = WL_RESET_MODE;
-	_status = WL_NO_SHIELD;
-	_init = 0;
-}
-
-uint8_t* macAddress(uint8_t *mac)
-{
-	m2m_wifi_get_mac_address(mac);
-	byte tmpMac[6], i;
-
-	m2m_wifi_get_mac_address(tmpMac);
-
-	for(i = 0; i < 6; i++)
-		mac[i] = tmpMac[5-i];
-
-	return mac;
-}
-
 int8_t scanNetworks()
 {
 	// Start scan:
@@ -463,159 +283,15 @@ void refresh(void)
 
 void lowPowerMode(void)
 {
-	m2m_wifi_set_sleep_mode(M2M_PS_H_AUTOMATIC, true);
+	m2m_wifi_set_sleep_mode(M2M_PS_H_AUTOMATIC, 1);
 }
 
 void maxLowPowerMode(void)
 {
-	m2m_wifi_set_sleep_mode(M2M_PS_DEEP_AUTOMATIC, true);
+	m2m_wifi_set_sleep_mode(M2M_PS_DEEP_AUTOMATIC, 1);
 }
 
 void noLowPowerMode(void)
 {
-	m2m_wifi_set_sleep_mode(M2M_NO_PS, false);
+	m2m_wifi_set_sleep_mode(M2M_NO_PS, 0);
 }
-
-int ping(IPAddress host, uint8_t ttl)
-{
-#ifdef CONF_PERIPH
-	// Network led ON (rev A then rev B).
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO16, 0);
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO5, 0);
-#endif
-
-	uint32_t dstHost = (uint32_t)host;
-	_resolve = dstHost;
-
-	if (m2m_ping_req((uint32_t)host, ttl, &ping_cb) < 0) {
-#ifdef CONF_PERIPH
-		// Network led OFF (rev A then rev B).
-		m2m_periph_gpio_set_val(M2M_PERIPH_GPIO16, 1);
-		m2m_periph_gpio_set_val(M2M_PERIPH_GPIO5, 1);
-#endif
-		//  Error sending ping request
-		return WL_PING_ERROR;
-	}
-
-	// Wait for success or timeout:
-	unsigned long start = millis();
-	while (_resolve == dstHost && millis() - start < 5000) {
-		m2m_wifi_handle_events(NULL);
-	}
-
-#ifdef CONF_PERIPH
-	// Network led OFF (rev A then rev B).
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO16, 1);
-	m2m_periph_gpio_set_val(M2M_PERIPH_GPIO5, 1);
-#endif
-
-	if (_resolve == dstHost) {
-		_resolve = 0;
-		return WL_PING_TIMEOUT;
-	} else {
-		int rtt = (int)_resolve;
-		_resolve = 0;
-		return rtt;
-	}
-}
-
-uint32_t getTime()
-{
-#ifdef WIFI_101_NO_TIME_H
-#warning "No system <time.h> header included, WiFi.getTime() will always return 0"
-	return 0;
-#else
-	tstrSystemTime systemTime;
-
-	_resolve = (uint32_t)&systemTime;
-
-	m2m_wifi_get_sytem_time();
-
-	unsigned long start = millis();
-	while (_resolve != 0 && millis() - start < 5000) {
-		m2m_wifi_handle_events(NULL);
-	}
-
-	time_t t = 0;
-
-	if (_resolve == 0 && systemTime.u16Year > 0) {
-		struct tm tm;
-
-		tm.tm_year = systemTime.u16Year - 1900;
-		tm.tm_mon = systemTime.u8Month - 1;
-		tm.tm_mday = systemTime.u8Day;
-		tm.tm_hour = systemTime.u8Hour;
-		tm.tm_min = systemTime.u8Minute;
-		tm.tm_sec = systemTime.u8Second;
-		tm.tm_isdst = -1;
-
-		t = mktime(&tm);
-	}
-
-	_resolve = 0;
-
-	return t;
-#endif
-}
-
-SOCKET clientSocketHdl;
-uint8 rxBuffer[256];
-/* Socket event handler.
- */
-void tcpClientSocketEventHandler(SOCKET sock, uint8 u8Msg, void * pvMsg)
-{
-	if(sock == clientSocketHdl)
-	{
-		if(u8Msg == SOCKET_MSG_CONNECT)
-		{
-			// Connect Event Handler.
-			tstrSocketConnectMsg *pstrConnect = (tstrSocketConnectMsg*)pvMsg;
-			if(pstrConnect->s8Error == 0)
-			{
-				// Perform data exchange.
-				uint8acSendBuffer[256];
-				uint16 u16MsgSize;
-				// Fill in the acSendBuffer with some data here
-				// send data
-				send(clientSocketHdl, acSendBuffer, u16MsgSize, 0);
-				// Recv response from server.
-				recv(clientSocketHdl, rxBuffer, sizeof(rxBuffer), 0);
-			}
-			else
-			{
-				printf("TCP Connection Failed\n");
-			}
-		}
-		else if(u8Msg == SOCKET_MSG_RECV)
-		{
-			tstrSocketRecvMsg *pstrRecvMsg = (tstrSocketRecvMsg*)pvMsg;
-			if((pstrRecvMsg->pu8Buffer != NULL) && (pstrRecvMsg->s16BufferSize > 0))
-			{
-				// Process the received message.
-				// Close the socket.
-				close(clientSocketHdl);
-			}
-		}
-	}
-}
-// This is the DNS callback. The response of gethostbyname is here.
-void dnsResolveCallback(uint8* pu8HostName, uint32 u32ServerIP)
-{
-	struct sockaddr_in strAddr;
-	if(u32ServerIP != 0)
-	{
-		clientSocketHdl = socket(AF_INET,SOCK_STREAM,u8Flags);
-		if(clientSocketHdl >= 0)
-		{
-			strAddr.sin_family = AF_INET;
-			strAddr.sin_port = _htons(443);
-			strAddr.sin_addr.s_addr = u32ServerIP;
-			connect(clientSocketHdl, (struct sockaddr*)&strAddr, sizeof(struct sockaddr_in));
-		}
-	}
-	else
-	{
-		printf("DNS Resolution Failed\n");
-	}
-}
-
